9e62643def22f2ad66c708a0e9c082db31c781b7
revengemiroz <mirozxy@gmail.com>
2020-03-29 23:54:42 +0600

    added new project

---
+++ frontendProjects-Showcase/index.js
@@ -0,0 +1,25 @@
+const projects = [
+    {
+        name: 'four-card-feature-section',
+        github: '',
+        instagram: ''
+    }
+]
+
+const containerEl = document.querySelector('.container')
+
+const display = () => {
+    projects.forEach(project => {
+        const CardHtml = `
+         <div class='card'>
+            <a href="/${project.name}/index.html">
+            <img src='//${project.name}/design/desktop-preview.jpg'></img>
+            ${project.name}
+            </a>
+          </div>`
+
+        containerEl.innerHTML = CardHtml
+    })
+}
+
+display()
