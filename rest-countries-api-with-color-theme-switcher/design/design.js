43c3ba4c64fe45e0fb8cd808b1f7e91b4041df87
revengemiroz <mirozxy@gmail.com>
2020-04-08 15:37:17 +0600

    added rest-countries-api-with-color-theme-switcher

---
+++ rest-countries-api-with-color-theme-switcher/design/design.js
@@ -0,0 +1,135 @@
+window.onload = () => {
+    //set the mode (dark or not)..
+    if (localStorage.getItem("dark-mode") == "false") {
+        localStorage.setItem("dark-mode", "true");
+    } else {
+        localStorage.setItem("dark-mode", "false");
+    }
+    performModeState();
+
+    //catch the country that user wants to see
+    const country = JSON.parse(localStorage.getItem("countryObject"));
+
+    //listener for clicking "back" button
+    document.querySelector(".back").addEventListener("click", () => {
+        window.location.pathname = "D:/vs code/FrontEnd-Mentor-Challenges/rest-countries-api-with-color-theme-switcher/";
+    });
+
+    //display the flag and infos about this country
+    const main = document.querySelector("main");
+    const img = document.createElement("img");
+    const infos = document.createElement("div");
+
+    img.src = country.flag;
+    img.alt = country.name;
+    img.classList.add("img-details");
+    infos.classList.add("info-details");
+
+    infos.innerHTML = `
+        <h1>${country.name}</h1>
+        <div class="info1-details">
+            <p><strong>Native Name:</strong> ${country.nativeName}</p>
+            <p><strong>Population:</strong> ${numberWithCommas(country.population)}</p>
+            <p><strong>Region:</strong> ${country.region}</p>
+            <p><strong>Sub Region:</strong> ${country.subregion}</p>
+            <p><strong>Capital:</strong> ${country.capital}</p>
+        </div>
+        <div class="info2-details">
+            <p><strong>Top Level Domain:</strong> ${country.topLevelDomain}</p>
+            <p><strong>Currencies:</strong> ${displayArray(country.currencies)}</p>
+            <p><strong>Languages:</strong> ${displayArray(country.languages)}</p>
+        </div>
+    `;
+    //add a paragraph to infos with the borders of that country... 
+    displayButtons(country.borders);
+    //add contents to the DOM
+    main.appendChild(img);
+    main.appendChild(infos);
+
+
+    //listener for clicking "Dark mode"
+    document.getElementById("dark").addEventListener("click", () => {
+        performModeState();
+    });
+
+
+    //function to perform dark mode or not..
+    function performModeState() {
+        if (localStorage.getItem("dark-mode") == "false") {
+            document.body.classList.add("body-dark-mode");
+            document.querySelector("nav").classList.add("dark-mode");
+            document.querySelector(".back").classList.add("dark-mode");
+            for (let i = 0; i < document.querySelectorAll(".borders button").length; i++) {
+                document.querySelectorAll(".borders button")[i].classList.add("dark-mode");
+                console.log("hi");
+            };
+            localStorage.setItem("dark-mode", "true");
+        } else {
+            document.body.classList.remove("body-dark-mode");
+            document.querySelector("nav").classList.remove("dark-mode");
+            document.querySelector(".back").classList.remove("dark-mode");
+            for (let i = 0; i < document.querySelectorAll(".btn-details").length; i++) {
+                document.querySelectorAll(".btn-details")[i].classList.remove("dark-mode");
+            };
+            localStorage.setItem("dark-mode", "false");
+        };
+    };
+
+
+    //function to display on DOM the content of an array of objects (with a name key) as a string
+    function displayArray(arr) {
+        const str = arr.reduce((acc, item) => acc + item.name + ", ", "");
+        return str.slice(0, str.length - 2);
+    };
+
+    //function to display on DOM the content of an array as a string of separate buttons
+    async function displayButtons(arr) {
+        if (arr.length > 0) {
+            let borders = arr.reduce((acc, item) => acc + item + ";", "");
+            borders = borders.slice(0, borders.length - 1);
+
+            const res = await fetch(`https://restcountries.eu/rest/v2/alpha?codes=${borders}`);
+            const data = await res.json();
+
+            let buttons = "";
+            data.forEach(country => buttons += `<button class="btn-details">${country.name}</button>`);
+            infos.innerHTML += `<p class="borders">Border Countries: ${buttons}</p>`;
+
+            //perform the state of card because on the reload it's not performing 
+            if (localStorage.getItem("dark-mode") == "true") {
+                for (let i = 0; i < document.querySelectorAll(".btn-details").length; i++) {
+                    document.querySelectorAll(".btn-details")[i].classList.add("dark-mode");
+                };
+            };
+
+            //listener for clicking a border
+            clickBorder();
+            return;
+        } else {
+            infos.innerHTML += `<p class="borders">Border Countries: None</p>`;
+            return;
+        }
+    };
+
+    //function to add listeners for clicking a border button
+    function clickBorder() {
+        document.querySelectorAll(".borders button").forEach(btn => {
+            btn.addEventListener("click", (e) => {
+                fetch(`https://restcountries.eu/rest/v2/name/${e.target.innerText}`)
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        localStorage.setItem("countryObject", JSON.stringify(data[0]));
+                        location.reload();
+                    });
+            });
+            return;
+        });
+    };
+
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+};
---
+++ rest-countries-api-with-color-theme-switcher/index.js
@@ -0,0 +1,169 @@
+window.onload = () => {
+    //set dark mode
+
+    if (localStorage.getItem('dark-mode')) {
+        if (localStorage.getItem('dark-mode') == 'false') {
+            localStorage.setItem('dark-mode', 'true')
+        }
+        else {
+            localStorage.setItem('dark-mode', 'false')
+        }
+        performModeState()
+    }
+    else {
+        localStorage.setItem('dark-mode', 'false')
+    }
+
+}
+let firstRender = true
+
+
+async function displayCountries(url) {
+
+    document.querySelector('main').innerHTML = `
+    <div class='loader'>
+    <span class="loading__anim"></span>
+    </div>
+    `
+
+    try {
+
+
+        const res = await fetch(url)
+        const data = await res.json()
+
+        //when the user fetches all the data stop the preloader
+        document.querySelector('main').innerHTML = ``
+
+        data.forEach(country => {
+
+            if (firstRender == true) {
+                const option = document.createElement('option')
+                option.value = country.name
+                document.querySelector('#searching').appendChild(option)
+            }
+
+
+
+            //html content for homepage cards
+            const cardsOutput = `
+        <h4>${country.name}</h4>
+        <p><strong>Population :</strong> ${numberWithCommas(country.population)}
+        <p><strong>Regoin : </strong>${country.region}</p>
+        <p><strong>Capital : </strong>${country.capital}</p>`
+
+            const card = document.createElement('div')
+            const img = document.createElement('img')
+            const div = document.createElement('div')
+            const main = document.querySelector('main')
+
+            card.classList.add('card')
+            div.classList.add('infos')
+
+            img.src = country.flag
+            img.alt = country.name
+
+            div.innerHTML = cardsOutput
+            card.appendChild(img)
+            card.appendChild(div)
+            main.appendChild(card)
+        });
+
+        if (localStorage.getItem('dark-mode') == 'true') {
+            for (let i = 0; i < document.querySelectorAll('.card').length; i++) {
+                document.querySelectorAll('.card')[i].classList.add('dark-mode')
+            }
+        }
+
+        firstRender = false
+        clickCountry(data)
+    }
+    catch (err) {
+        document.querySelector("main").innerHTML = "<h1 style='color:hsl(0, 0%, 52%)'>No country by that Name..</h1>";
+    }
+
+}
+
+displayCountries(`https://restcountries.eu/rest/v2/all`)
+
+//filter by region
+const region = document.querySelector('.region')
+region.addEventListener('change', () => {
+    if (region.value === '') {
+        displayCountries("https://restcountries.eu/rest/v2/all");
+    }
+    else {
+        displayCountries(`https://restcountries.eu/rest/v2/region/${region.value.toLowerCase()}`);
+    }
+})
+
+//search for a specific country
+const search = document.querySelector('.search')
+search.addEventListener('change', () => {
+    if (search.value === '' && region.value != '') {
+        displayCountries(`https://restcountries.eu/rest/v2/region/${region.value.toLowerCase()}`);
+    }
+    else if (search.value === '' && region.value === '') {
+        displayCountries("https://restcountries.eu/rest/v2/all");
+    }
+    else {
+        displayCountries(`https://restcountries.eu/rest/v2/name/${search.value.toLowerCase()}`);
+    }
+})
+
+
+//click a country
+function clickCountry(data) {
+    document.querySelector('main').addEventListener('click', e => {
+        if (e.target.classList.contains('infos') || e.target.tagName == 'IMG') {
+            const countryObject = JSON.stringify(data.find(country => country.name == e.target.alt))
+
+            localStorage.setItem('countryObject', countryObject)
+            window.location.pathname = `rest-countries-api-with-color-theme-switcher/details/details.html`
+        }
+    })
+}
+
+
+
+
+//dark mode toogle
+document.getElementById('dark').addEventListener('click', () => {
+    performModeState()
+    console.log('ss')
+})
+
+function performModeState() {
+    if (localStorage.getItem('dark-mode') == 'false') {
+        document.body.classList.add('body-dark-mode')
+        document.querySelector('nav').classList.add('dark-mode')
+
+        for (let i = 0; i < document.querySelectorAll('input').length; i++) {
+            document.querySelectorAll('input')[i].classList.add('dark-mode')
+        }
+
+        for (let i = 0; i < document.querySelectorAll('.card').length; i++) {
+            document.querySelectorAll('.card')[i].classList.add('dark-mode')
+        }
+
+        localStorage.setItem('dark-mode', 'true')
+    }
+    else {
+        document.body.classList.remove('body-dark-mode')
+        document.querySelector('nav').classList.remove('dark-mode')
+
+        for (let i = 0; i < document.querySelectorAll('input').length; i++) {
+            document.querySelectorAll('input')[i].classList.remove('dark-mode')
+        }
+
+        for (let i = 0; i < document.querySelectorAll('.card').length; i++) {
+            document.querySelectorAll('.card')[i].classList.remove('dark-mode')
+        }
+
+        localStorage.setItem('dark-mode', 'false')
+    }
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
