68d55a7736174c19eb7b5fd70395621096894aa6
revengemiroz <mirozxy@gmail.com>
2020-04-13 15:51:38 +0600

    modiefied index.js v2

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.getElementById('modal_container');
+const video = document.querySelector('.video')
+
+
+close.addEventListener('click', () => {
+
+    console.log('close')
+    video.innerHTML = `<video class="video" width="320" height="240">
+    <source
+        src="Jack-Black.mp4"
+        type="video/mp4">
+    <source src="movie.ogg" type="video/ogg">
+    Your browser does not support the video tag.
+</video>`
+    modal.classList.remove('show');
+});
+
+
+
+
+
 const projects = [
     {
         name: 'four-card-feature-section',
@@ -88,7 +111,37 @@ const formatNames = (n) => {
 
 
 display()
+var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var current = 0;
 
+var keyHandler = function (event) {
+
+    // If the key isn't in the pattern, or isn't the current key in the pattern, reset
+    if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+        current = 0;
+        return;
+    }
+
+    // Update how much of the pattern is complete
+    current++;
+
+    // If complete, alert and reset
+    if (pattern.length === current) {
+        current = 0;
+        modal.classList.add('show');
+        video.innerHTML = `<video autoplay class="video" width="320" height="240" loop>
+        <source
+            src="Jack-Black.mp4"
+            type="video/mp4">
+        <source src="movie.ogg" type="video/ogg">
+        Your browser does not support the video tag.
+    </video>`
+    }
+
+};
+
+// Listen for keydown events
+document.addEventListener('keydown', keyHandler, false);
 
 
 // <div class='imageScale'>
